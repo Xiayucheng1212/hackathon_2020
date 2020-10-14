420385af030472c8824a3ee55b1f94f13b879f52
YoyoShih <64921618+YoyoShih@users.noreply.github.com>
2020-10-14 21:39:38 +0800

    finish fp functions
    click : turn to the certain page

--- main_page_anim.js
+++ main_page_anim.js
@@ -4,7 +4,7 @@ circle.onmouseenter = function(){
     if(!flag){
         circle.classList.add('circle_anim');
         for(var i=0;i<3;i++){
-            document.getElementsByClassName('fp_titles').item(i).classList.add('fp_titles_appear');
+            document.getElementsByClassName('fp_titles').item(i).classList.add('fp_titles_appear_delay');
         }
         flag = true;
     }
@@ -13,6 +13,7 @@ var proj_map = document.getElementById('proj_map'),
     proj_list = document.getElementById('proj_list'),
     account = document.getElementById('account');
 proj_map.onmouseenter = function(){
+    proj_map.children.item(0).classList.remove('fp_titles_appear_delay');
     if(flag){
         proj_map.children.item(0).classList.remove('fp_titles_appear');
         proj_map.children.item(1).classList.add('fp_intro_appear');
@@ -24,8 +25,12 @@ proj_map.onmouseleave = function(){
         proj_map.children.item(0).classList.add('fp_titles_appear');
     }
 }
+proj_map.onclick = function(){
+    open("travel_projects_map.html");
+}
 
 proj_list.onmouseenter = function(){
+    proj_list.children.item(0).classList.remove('fp_titles_appear_delay');
     if(flag){
         proj_list.children.item(0).classList.remove('fp_titles_appear');
         proj_list.children.item(1).classList.add('fp_intro_appear');
@@ -37,8 +42,12 @@ proj_list.onmouseleave = function(){
         proj_list.children.item(0).classList.add('fp_titles_appear');
     }
 }
+proj_list.onclick = function(){
+    open("travel_projects_list.html");
+}
 
 account.onmouseenter = function(){
+    account.children.item(0).classList.remove('fp_titles_appear_delay');
     if(flag){
         account.children.item(0).classList.remove('fp_titles_appear');
         account.children.item(1).classList.add('fp_intro_appear');
@@ -49,4 +58,7 @@ account.onmouseleave = function(){
         account.children.item(1).classList.remove('fp_intro_appear');
         account.children.item(0).classList.add('fp_titles_appear');
     }
+}
+account.onclick = function(){
+    open("account.html");
 }

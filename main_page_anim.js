295e9fec355412481ddaf510e7dbd7a632908303
YoyoShih <64921618+YoyoShih@users.noreply.github.com>
2020-10-14 19:06:05 +0800

    create fp_titles
    create fp_titles and animations

--- main_page_anim.js
+++ main_page_anim.js
@@ -1,4 +1,7 @@
 var circle = document.getElementById('fp_circle');
 circle.onmouseenter = function(){
     circle.classList.add('circle_anim');
+    for(var i=0;i<3;i++){
+        document.getElementsByClassName('fp_titles').item(i).classList.add('fp_titles_anim');
+    }
 }
